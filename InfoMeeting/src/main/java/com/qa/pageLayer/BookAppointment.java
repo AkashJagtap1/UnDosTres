619ce6b79aebef0a39b45c007a86dae6a4c05d1a
AkashJagtap1 <112076016+AkashJagtap1@users.noreply.github.com>
2022-09-08 13:23:20 +0530

    Deleted hi
    Deleted hi

--- InfoMeeting/src/main/java/com/qa/pageLayer/BookAppointment.java
+++ InfoMeeting/src/main/java/com/qa/pageLayer/BookAppointment.java
@@ -11,7 +11,7 @@ public class BookAppointment extends TestBase {
 
 	public BookAppointment (){
 		PageFactory.initElements(driver,this);
-	System.out.println("hii");
+	
 	}
 
 	@FindBy(xpath="//a[@class='right']")
