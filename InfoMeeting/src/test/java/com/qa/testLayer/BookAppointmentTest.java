a77a7096da11c91e93b8200b2e5aac40b024647e
AkashJagtap1 <112076016+AkashJagtap1@users.noreply.github.com>
2022-09-08 15:34:33 +0530

    book
    appointment booking

--- InfoMeeting/src/test/java/com/qa/testLayer/BookAppointmentTest.java
+++ InfoMeeting/src/test/java/com/qa/testLayer/BookAppointmentTest.java
@@ -2,12 +2,12 @@ package com.qa.testLayer;
 
 import org.testng.annotations.Test;
 
-
+import com.qa.pageLayer.BookAppointment;
 import com.qa.pageLayer.FindPatientRecord;
 import com.qa.pageLayer.HomePage;
 import com.qa.testBase.TestBase;
 
-public class FindPatientRecordTest extends TestBase{
+public class BookAppointmentTest extends TestBase {
 
 	@Test
 	public void verifyPatientFinding () throws InterruptedException {
@@ -20,9 +20,16 @@ public class FindPatientRecordTest extends TestBase{
 		findPatient.clickOnPatientName();
 		Thread.sleep(3000);
 
+		BookAppointment book = new BookAppointment();
+		book.bookAnAppointment();
+		book.selectServiceType();
+		Thread.sleep(3000);
+		book.clickOnCheckbox();
+		Thread.sleep(2000);
+		book.clickOnSearchBtn();
+		Thread.sleep(3000);
+
 		home.logoutBtn();
 		Thread.sleep(2000);
 	}
-
-
 }
